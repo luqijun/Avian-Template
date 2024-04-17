b358e281c0664425fa4c787ecc103fb688b52adf
lqjun <1120948897@qq.com>
2024-04-17 11:14:49 +0800

    添加index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import type {IUser, IContact, IPreviewData, IAttachment,
+  IRecording, IMessage, IConversation, IContactGroup,
+   INotification, ISettings, ICall, IEmoji
+} from './src/types'
+
+// import { Chat } from './src/components/views/HomeView/Chat/Chat.vue'
+// import { Sidebar } from './src/components/views/HomeView/Sidebar/Sidebar.vue'
+
+// export { Chat, Sidebar }
+
+export {IUser, IContact, IPreviewData, IAttachment,
+  IRecording, IMessage, IConversation, IContactGroup,
+   INotification, ISettings, ICall, IEmoji
+}
