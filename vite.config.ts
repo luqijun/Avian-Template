30b63308e4d3b2e7648a5bd6c328d8d5d7727b50
lqjun <1120948897@qq.com>
2024-04-18 00:01:53 +0800

    配置打包

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,41 @@ import { defineConfig } from "vite";
 import alias from "@rollup/plugin-alias";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import dtsPlugin from "vite-plugin-dts";
 
 const rootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), alias()],
+
+  build: {
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+    lib: {
+      name: "avian",
+      entry: resolve(__dirname, "src/main.ts")
+    }
+  },
+  plugins: [
+    vue({
+      script: {
+        defineModel: true
+      }
+    }),
+    vueJsx(),
+    alias(),
+    dtsPlugin({
+      outDir: ["dist/"],
+      include: ["packages/**/*.ts"]
+    })
+  ],
   resolve: {
     alias: {
       "@src": resolve(rootDir, "src"),
